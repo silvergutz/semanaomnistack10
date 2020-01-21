05119962d28225da76b1237085b6b1740d83c6cf
Tiago Vergutz <silver@girafacomunicacao.com.br>
2020-01-21 15:03:38 -0300

    Concluido basico da aplicacao web, com cadastro, e listagem de devs

--- web/src/App.js
+++ web/src/App.js
@@ -1,106 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-// Componente
-// Estado
-// Propriedade
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (err) => {
-        console.error(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
-  }, []); 
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    if (response.data && response.data.success && response.data.resource) {
+      setDevs([...devs, response.data.resource]);
+    }
+  };
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input type="text" name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input type="text" name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="text" name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="text" name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-        <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes"/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>Node.js, ReactJS, ReactNative</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes"/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>Node.js, ReactJS, ReactNative</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes"/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>Node.js, ReactJS, ReactNative</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Diego Fernandes"/>
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>Node.js, ReactJS, ReactNative</span>
-              </div>
-            </header>
-            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
-            <a href="https://github.com/diego3g">Acessar perfil no GitHub</a>
-          </li>
+          { devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          )) }
         </ul>
       </main>
     </div>
